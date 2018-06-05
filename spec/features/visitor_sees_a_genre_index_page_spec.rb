7646be142d3feea39a7f8af55c5ffdcd757e9f3d
JerrelMitchell <jerrelmitchell@ymail.com>
2018-06-05 09:47:10 -0600

    add in test for visitor index categories

---
+++ spec/features/visitor_sees_a_genre_index_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "visits categories index page" do
+  before(:each) do
+    director = Director.create!(name: 'Someone!')
+    movie1 = director.movies.create!(title: 'Shawshank Redemption', description: 'Tim Robbins in jail')
+    movie2 = director.movies.create!(title: 'Mary Poppins', description: 'Tim Robbins in jail')
+    @genre1 = movie1.genres.create!(name: 'Sci-fi')
+    @genre2 = movie2.genres.create!(name: 'Action')
+  end
+  context "as visitor" do
+    it "allows visitor to see all genres" do
+
+      visit genres_path
+
+      expect(page).to_not have_content("Create New Genre")
+
+      expect(page).to have_content("All Genres")
+      expect(page).to have_content(@genre1.name)
+      expect(page).to have_content(@genre2.name)
+    end
+  end
+end
