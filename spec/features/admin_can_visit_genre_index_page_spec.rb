8b31df96ffc486f4cc6fd1edc1032ad3ed805eed
JerrelMitchell <jerrelmitchell@ymail.com>
2018-06-05 09:31:18 -0600

    create model and spec tests

---
+++ spec/features/admin_can_visit_genre_index_page_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "User visits genres index page" do
+  before(:each) do
+    director = Director.create!(name: 'Someone!')
+    movie1 = director.movies.create!(title: 'Shawshank Redemption', description: 'Tim Robbins in jail')
+    movie2 = director.movies.create!(title: 'Mary Poppins', description: 'Tim Robbins in jail')
+    @genre1 = movie1.genres.create!(name: 'Sci-fi')
+    @genre2 = movie2.genres.create!(name: 'Action')
+  end
+  context "as admin" do
+    it "allows admin to see all genres" do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_genres_path
+
+      expect(page).to have_content("Admin Genres")
+      expect(page).to have_content(@genre1.name)
+      expect(page).to have_content(@genre2.name)
+    end
+
+    it 'sees a form to create a new genre' do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit admin_genres_path
+
+      expect(page).to have_content("Create New Genre")
+    end
+  end
+
+  context "as default user" do
+    it "does not allow default user to see admin genres index" do
+      user = User.create(username: "Ian", password: "password123", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_genres_path
+
+      expect(page).to_not have_content("Admin Genres")
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end
