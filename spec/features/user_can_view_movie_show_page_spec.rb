b7e958593eba5b1c6bcbd3d26552ef9e17760fd7
JerrelMitchell <jerrelmitchell@ymail.com>
2018-06-05 10:13:45 -0600

    add test for movie show page genres

--- spec/features/user_can_view_movie_show_page_spec.rb
+++ spec/features/user_can_view_movie_show_page_spec.rb
@@ -12,4 +12,29 @@ describe "user sees one movie" do
     expect(page).to have_content(movie.title)
     expect(page).to have_content(movie.description)
   end
+  it 'sees genres listed only for this movie ' do
+    director = Director.create(name: 'George Lucas')
+    movie1 = director.movies.create(title:'Star Wars Episode 4', description: 'A New Hope')
+    movie2 = director.movies.create(title:'Dude Wheres My Car', description: 'Dude')
+    genre1 = movie1.genres.create!(name: 'Sci-fi')
+    genre2 = movie1.genres.create!(name: 'Fiction')
+    genre3 = movie2.genres.create!(name: 'Comedy')
+    genre4 = movie2.genres.create!(name: 'Rom-Com')
+    genre_message = "Genres for this Movie:"
+
+    visit movie_path(movie1.slug)
+
+    expect(page).to_not have_content(genre3.name)
+    expect(page).to_not have_content(genre4.name)
+
+    expect(page).to have_content(genre_message)
+    expect(page).to have_content(genre1.name)
+    expect(page).to have_content(genre2.name)
+  end
 end
+
+
+# As a Visitor,
+#   When I visit a movie show page,
+#     I see genre names under a heading that says "Genres for this Movie:"
+#       and the genres listed are only the genres associated with this movie.
